5e7be72e9089dc1f39e9032873e579046d40331c
Klender Carrasco <36539682+MarcosKlender@users.noreply.github.com>
2024-12-26 23:40:22 -0500

    Add Vercel JSON Update Landing page styles

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,10 +5,10 @@ export function Landing() {
     const { countdown } = useCountdown(5);
 
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center text-center">
             <h1 className="page-title">Bienvenido a Relatos de Papel</h1>
             <p className="pb-5 text-lg">¡Tu destino literario te espera!</p>
-            <p className="text-sm">Redirigiendo en {countdown} segundos</p>
+            <p className="text-sm">Redirigiendo en {countdown}s</p>
             <img src={LandingImage} alt="Mujer leyendo un libro" className='w-full py-10 sm:w-1/2 md:w-1/3' />
         </div>
     );
